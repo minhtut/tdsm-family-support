60a9e305dd82f61854ccaae794d2202415fcf479
Min Htut <htutm@go.mts.com>
2020-11-02 15:07:06 -0600

    Added translated link.

--- src/views/Resource.vue
+++ src/views/Resource.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="resource">
-    <p>{{ $t("page-resource") }}</p>
+    <v-container>
+      <a v-if="$i18n.locale == 'EN'" style="text-decoration: none" href="https://www.health.state.mn.us/diseases/coronavirus/" target="_blank">{{ $t("mndoh-covid19") }}</a>
+      <a v-else style="text-decoration: none" href="https://translate.google.com/translate?hl=en&sl=en&tl=my&u=https%3A%2F%2Fwww.health.state.mn.us%2Fdiseases%2Fcoronavirus%2F" target="_blank">{{ $t("mndoh-covid19") }}</a>
+    </v-container>
   </div>
 </template>
 
 <script>
 export default {
   name: "Resource",
+  data: () => ({
+      items: [
+        { name: 'Real-Time', icon: 'mdi-apps' },
+        { name: 'Audience', icon: 'mdi-account' },
+        { name: 'Conversions', icon: 'mdi-flag' },
+      ],
+    }),
 };
 </script>
