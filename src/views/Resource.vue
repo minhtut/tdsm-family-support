59a11471e554e3d7a9155c8402c9b89798fe19ce
Min Htut <htutm@go.mts.com>
2020-11-02 16:01:40 -0600

    Added HHS.

--- src/views/Resource.vue
+++ src/views/Resource.vue
@@ -4,6 +4,20 @@
       <a
         v-if="$i18n.locale == 'EN'"
         style="text-decoration: none"
+        href="https://www.hhs.gov/"
+        target="_blank"
+        >{{ $t("usd-hhs") }}</a
+      >
+      <a
+        v-else
+        style="text-decoration: none"
+        href="https://translate.google.com/translate?hl=en&sl=en&tl=my&u=https%3A%2F%2Fwww.hhs.gov"
+        target="_blank"
+        >{{ $t("usd-hhs") }}</a
+      ><br /><br />
+      <a
+        v-if="$i18n.locale == 'EN'"
+        style="text-decoration: none"
         href="https://www.health.state.mn.us/diseases/coronavirus/"
         target="_blank"
         >{{ $t("mndoh-covid19") }}</a
@@ -22,7 +36,6 @@
 <script>
 export default {
   name: "Resource",
-  data: () => ({
-  }),
+  data: () => ({}),
 };
 </script>
